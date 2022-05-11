508da5bae6a8a7dc464fcf104aeaba0aeed38752
jusong23 <jusong23@naver.com>
2022-05-12 08:49:33 +0900

    podInstall

---
+++ FASTCAMPUS/Project/newWeather/newWeather/weatherOverview.swift
@@ -0,0 +1,12 @@
+//
+//  weatherOverview.swift
+//  newWeather
+//
+//  Created by 이주송 on 2022/05/11.
+//
+
+import Foundation
+
+struct weatherOverview: Codable {
+    let weather
+}
---
+++ FASTCAMPUS/Project/newWeather/weatherOverview.swift
@@ -0,0 +1,8 @@
+//
+//  weatherOverview.swift
+//  newWeather
+//
+//  Created by 이주송 on 2022/05/11.
+//
+
+import Foundation
